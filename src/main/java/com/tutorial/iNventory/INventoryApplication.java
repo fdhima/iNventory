28d690fbef7763222fca3cb5ab12cf93d90d3eec
dflorian251 <dflorian251@gmail.com>
2026-04-23 10:40:30 +0300

    Switching to Java Spring

---
+++ src/main/java/com/tutorial/iNventory/INventoryApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.iNventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class INventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(INventoryApplication.class, args);
+	}
+
+}
